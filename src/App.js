c8a7038be1454124b69a84cf1b2e4c29230e2d4d
tedo <tjanelidze@unisens.ge>
2022-07-20 16:16:13 +0400

    adding cart context and shoping icon as well as cart dropdown

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return <h1>hello</h1>;
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import reportWebVitals from './reportWebVitals';
+import { ProductProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
